187444a88a27fee21e25eb636a94b84d666c6d75
Andymic <Andymic12@gmail.com>
2015-02-25 07:55:05 -0500

    added shell module pattern, event handlers, DOM manipulation methods and started uirAnchor pattern for history

--- js/spa.js
+++ js/spa.js
@@ -14,11 +14,7 @@ Root namespace module
 
 var spa=(function(){
 	var initModule=function($container){
-		$container.html(
-			'<h1 style="display: inline-block; margin:25px;">'
-			+'hello world'
-			+'</h1>'
-			);
+		spa.shell.initModule($container);
 	};
 
 	return { initModule: initModule};

d2c18fe5d66805e576af7d46e05e6ec05eb07856
Andymic <Andymic12@gmail.com>
2015-03-14 15:35:23 -0400

    added login, logout modules and subscriber events

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -16,7 +16,12 @@
 spa.fake=(function() {
     'use strict';
 
-    var getPeopleList;
+    var getPeopleList,fakeIdSerial, makeFakeId, mockSio;
+    fakeIdSerial = 5,
+
+    makeFakeId = function(){
+        return 'id_'+ String(fakeIdSerial++);
+    }
     getPeopleList = function(){
     	return[
     		{
@@ -45,5 +50,35 @@ spa.fake=(function() {
     		}
     	];
     };
-	return {getPeopleList : getPeopleList};
+
+    mockSio = (function(){
+        var on_sio, emit_sio, callback_map={};
+        on_sio = function(msg_type, callback){
+            callback_map[msg_type]=callback;
+        };
+
+        emit_sio = function(msg_type, data){
+            //respond to 'adduser' event with 'userupdate'
+            //callback after a 3s delay
+
+            if(msg_type==='adduser' && callback_map.userupdate){
+                setTimeout(function(){
+                    callback_map.userupdate(
+                        [{
+                            _id : makeFakeId(),
+                            name : data.name,
+                            css_map : data.css_map
+                        }]
+                        );
+                }, 3000);
+            }
+        };
+
+        return {emit : emit_sio, on : on_sio};
+    }());
+
+	return {
+        getPeopleList : getPeopleList,
+        mockSio : mockSio
+    };
 }());
--- js/spa.model.js
+++ js/spa.model.js
@@ -19,11 +19,13 @@ spa.model=(function() {
 	configMap = { anon_id : 'a0'},
 	stateMap = {
 		anon_user : null,
+		cid_serial: 0,
 		people_cid_map : {},
-		people_db : TAFFY()
+		people_db : TAFFY(),
+		user: null
 	},
 	isFakeData = true,
-	personProto, makePerson, people, initModule;
+	personProto, makeCid, clearPeopleDb, completeLogin, makePerson, removePerson, people, initModule;
 
 	personProto = {
 		get_is_user : function(){
@@ -32,6 +34,33 @@ spa.model=(function() {
 		get_is_anon : function(){
 			return this.cid ===stateMap.anon_user.cid;
 		}
+	};
+
+	makeCid = function()
+	{
+		return 'c' + String(stateMap.cid_serial++);
+	};
+
+	clearPeopleDb = function(){
+		var user = stateMap.user;
+		stateMap.people_db = TAFFY();
+		stateMap.people_cid_map = {};
+		if(user){
+			stateMap.people_db.insert(user);
+			stateMap.people_cid_map[user.cid] = user;
+		}
+	};
+
+	completeLogin = function(user_list){
+		var user_map = user_list[0];
+		delete stateMap.people_cid_map[user_map.cid];
+		stateMap.user.cid = user_map._id;
+		stateMap.user.id = user_map._id;
+		stateMap.user.css_map = user_map.css_map;
+		stateMap.people_cid_map[user_map._id]=stateMap.user;
+
+		//when we add chat, we should join here
+		$.gevent.publish('spa-login', [stateMap.user]);
 	};
 
 	makePerson = function(person_map){
@@ -55,14 +84,81 @@ spa.model=(function() {
 		stateMap.people_cid_map[cid]=person;
 
 		stateMap.people_db.insert(person);
-		alert(stateMap.people_db.length);
 		return person;
 	};
 
-	people = {
-		get_db : function(){return stateMap.people_db;},
-		get_cid_map : function(){return stateMap.people_cid_map;}
+	removePerson = function(person){
+		if(!person){return false;}
+		//can't remove anonymous erson
+		if(person.id==configMap.anon_id){
+			return false;
+		}
+
+		stateMap.people_db({cid : person.cid}).remove();
+		if(person.cid){
+			delete stateMap.people_cid_map[person.cid];
+		}
+		return true;
 	};
+
+	people = (function(){
+		var get_by_cid, get_db, get_user, login, logout;
+
+		get_by_cid = function(cid){
+			return stateMap.people_cid_map[cid];
+		};
+
+		get_db = function(){
+			return stateMap.people_db;
+		};
+
+		get_user = function(){
+			return stateMap.user;
+		};
+
+		login = function(name){
+			var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+			stateMap.user = makePerson({
+				cid : makeCid(),
+				css_map : {top : 25, left : 25, 'background-color':'#8f8'},
+				name : name
+			});
+
+			sio.on('userupdate',completeLogin);
+
+			sio.emit('adduser',{
+				cid : stateMap.user.cid,
+				css_map : stateMap.user.css_map,
+				name : stateMap.user.name
+			});
+		};
+
+		logout = function(){
+			var is_removed, user = stateMap.user;
+			//when we add chat, we should leave the chatroom here
+
+			is_removed = removePerson(user);
+			stateMap.user = stateMap.anon_user;
+
+			$.gevent.publish('spa-logout', [user]);
+
+			return is_removed;
+		};
+
+		return {
+			get_by_cid : get_by_cid,
+			get_db : get_db,
+			get_user : get_user,
+			login : login,
+			logout : logout
+		};
+	}());
+
+	// people = {
+	// 	get_db : function(){return stateMap.people_db;},
+	// 	get_cid_map : function(){return stateMap.people_cid_map;}
+	// };
 
 	initModule = function(){
 		var i, people_list, person_map;
